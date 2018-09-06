dd5d5b67433146091851e0e47b25f4f6c5bf329c
PearKMA <42665181+PearKMA@users.noreply.github.com>
2018-09-06 20:35:55 +0700

    Update and rename Lab2.5 to Lab31e.s

---
+++ Lab31e.s
@@ -0,0 +1,35 @@
+; Khai bao doan du lieu
+	AREA    RESET, DATA, READONLY
+		DCD  0x20001000     ; stack pointer value when stack is empty
+		DCD  Reset_Handler  ; reset vector
+  
+      ALIGN
+ 
+; Khai bao doan ma lenh
+          AREA    MYCODE, CODE, READONLY
+ 
+   	  ENTRY ;Khai bao diem vao chuong trinh
+   	  EXPORT Reset_Handler
+	  
+Reset_Handler
+;-------------------------------------------------------------------------
+; Bat dau doan code (Diem vao cua chuong trinh)---------------------------
+;Tinh x^n
+soN equ 9
+soX equ 3
+start
+	ldr r2, =soN
+	ldr r3, =soX
+	mov r0,r3
+	mov r1,#1
+	bl tinhToan
+tinhToan proc
+	cmp r1,r2
+	bgt thoat
+	mul r0,r3
+	add r1,#1
+	bl tinhToan
+thoat
+	nop
+stop b stop
+end
